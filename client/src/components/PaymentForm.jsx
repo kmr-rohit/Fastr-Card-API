d91c817946781296b85b0c887bb5b1cd3be3d158
kmr-rohit <rk972006@student.nitw.ac.in>
2024-02-18 00:18:25 +0530

    Upi Payment  Added

--- client/src/components/PaymentForm.jsx
+++ client/src/components/PaymentForm.jsx
@@ -1,6 +1,6 @@
 import React ,{useState} from 'react'
+import axios from "axios";
 const baseApiUrl = 'http://localhost:3000/api';
-
 function cardForm() {
   // create a card form to accept card details with tailwind css 
   return (
@@ -16,8 +16,6 @@ function cardForm() {
       </form>
     </div>
   )
-
-  
 }
 
  function PaymentForm() {
@@ -80,7 +78,166 @@ async function makeDiffPayment() {
   setTransactionStatusArray(transactionStatus.status);
 }
 
+async function makeUPIpayment() {
+  try {
+    const { data: { key } } = await axios.get("http://www.localhost:3000/api/getkey");
+    const URL = baseApiUrl + '/makeupipayment';
+    const {data:{order}} = await axios.get(URL)
+    console.log(order);
+    const options = {
+      key,
+      amount: order.amount,
+      currency: "INR",
+      name: "Ajeet",
+      description: "Payment",
+      image: "",
+      config : {
+          display: {
+        
+            hide: [
+              {
+                method:"card",
+              },
+              {
+                method:"wallet",
+              },
+              {
+                method:"netbanking",
+              },
+              {
+                method:"paylater",
+              }
+            ],
+        
+            sequence: ["block.code"], // The sequence in which blocks and methods should be shown
+        
+            preferences: {
+              show_default_blocks: true // Should Checkout show its default blocks?
+            }
+          }
+        }, 
+      callback_url: baseApiUrl + "/verifyupipayment",
+      order_id: order.id,
+      prefill: {
+          name: "Ajeet Verma",
+          email: "ajeet@gmail.com",
+          contact: "1234567890"
+      },
+      notes: {
+          "address": "IIIT Naya Raipur"
+      },
+      theme: {
+          "color": "#121212"
+      }
+  };
+
+    const razorpay = new window.Razorpay(options);
+    razorpay.open();
+  } catch (error) {
+    console.error("Error:", error);
+  }
+  
+}
+async function makeCardAndUPIPayment() {
+  try {
+    const cardPaymentUrl = baseApiUrl + '/makepayment' + '?payment_method=pm_card_visa';
+    // Encapsulate card payment in a functio
+    async function handleCardPayment() {
+      const response = await fetch(cardPaymentUrl);
+      const cardPayment = await response.json();
+      var newWindow = window.open(cardPayment.url, '_blank');
+      // Set a timeout to close the window after 7 seconds to proceed with the 3DS authentication
+      await new Promise(resolve => setTimeout(() => {
+        newWindow.close();
+        resolve();
+      }, 7000));
+      return cardPayment;
+    }
+
+    // Encapsulate UPI payment in a function
+    async function handleUPIPayment() {
+      try {
+        const { data: { key } } = await axios.get(`${baseApiUrl}/getkey`);
+        const URL = `${baseApiUrl}/makeupipayment`;
+        const { data: { order } } = await axios.get(URL);
+        console.log(order);
+    
+        // Open a new popup window for the Razorpay payment
+        const paymentWindow = window.open(`${baseApiUrl}/razorpayPayment?order_id=${order.id}&&key=${key}`, 'Razorpay', 'width=800,height=600');
+        
+        // Return a new promise that resolves when the payment status is fetched
+        return new Promise(async (resolve, reject) => {
+          // Poll to check if the window is closed
+          const pollTimer = window.setInterval(async function() {
+            if (paymentWindow.closed !== false) { // !== is required for compatibility with Opera
+              window.clearInterval(pollTimer);
+    
+              // Verify the payment status
+              try {
+                const response = await fetch(`${baseApiUrl}/upipaymentstatus`);
+                const paymentStatus = await response.json();
+                console.log(paymentStatus);
+                resolve(paymentStatus); // Resolve the outer promise with the payment status
+              } catch (error) {
+                reject(error); // Reject the outer promise if there's an error
+              }
+            }
+          }, 200);
+        });
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    // Execute both payment functions concurrently
+    const responses = await Promise.all([handleCardPayment(), handleUPIPayment()]);
+    //console.log(responses);
+    // set the transaction status in the state
+    const UPiStatus = responses[1];
+    const UPIStatusStructured = {payment_method : "UPI"  , paymentIntentId: UPiStatus.reference, status: UPiStatus.success ? 'succeeded' : 'failed'};
+    
+    const cardStatus = await checkStatusandTakeAction([responses[0]]);
+    console.log(cardStatus);
+    setOpenModal(true);
+    setTransactionStatusArray([cardStatus.status[0], UPIStatusStructured]);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
 async function checkStatusandTakeAction(responses) {
+  if(responses.length === 1) {
+    const status = await fetch(baseApiUrl + '/paymentstatus', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({paymentid1 : responses[0].paymentIntentId})
+    });
+    const statusResponse = await status.json();
+
+    if(statusResponse.status[0] === 'requires_capture') {
+      const capturePayment = await fetch(baseApiUrl + '/capturepayment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({paymentid1 : responses[0].paymentIntentId})
+      });
+      const captureResponse = await capturePayment.json();
+      statusResponse.status = captureResponse.status;
+    } else {
+      const cancelPayment = await fetch(baseApiUrl + '/cancelpayment', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({paymentid1 : responses[0].paymentIntentId})
+      });
+      const cancelResponse = await cancelPayment.json();
+      statusResponse.status = cancelResponse.status;
+    }
+    return statusResponse;
+  }
   //console.log(responses);
   const status = await fetch(baseApiUrl + '/paymentstatus', {
     method: 'POST',
@@ -127,10 +284,11 @@ async function checkStatusandTakeAction(responses) {
   return (
     
     <div className='m-[50px]'>
-      {/* {cardForm()} */}
+       {/* {cardForm()}  */}
       <button onClick={makeSamePayment} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Pay Now(Same Brand - 2 cards)</button>
       <button onClick={makeDiffPayment} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Pay Now(2 Cards - 1 Failure)</button>
-
+      {/* <button onClick={makeUPIpayment} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Pay Now(1 upi)</button> */}
+      <button onClick={makeCardAndUPIPayment} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'>Pay Now(1Card & 1 UPI)</button>
       {openModal && (
         <div className="fixed inset-0 z-50 overflow-auto bg-gray-900 bg-opacity-50 flex items-center justify-center">
           <div className="bg-white p-8 max-w-md mx-auto rounded-md text-center">
@@ -138,7 +296,7 @@ async function checkStatusandTakeAction(responses) {
             <p className="mb-4">{transactionStatusArray && 
               transactionStatusArray.map((status, index) => (
                 <>
-                <p key={index}>Payment Status for payment with {status.paymentIntentId} : {status.status} </p>
+                <p key={index}>{status.payment_method} Payment with PaymentID {status.paymentIntentId} : {status.status} </p>
                 </>
               ))
             }</p>
--- server/index.js
+++ server/index.js
@@ -4,9 +4,16 @@ var cors = require('cors')
 var bodyParser = require('body-parser')
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const crypto = require('crypto');
+const Razorpay = require('razorpay');
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET
+});
 app.use(cors())
-app.use(bodyParser.json())
-
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+var upiPaymentResult = {};
 app.get('/api/makepayment', async (req, res) => {
     // Making a single Successfull Payment from Stripe API with Express Server has multiple steps
 
@@ -54,6 +61,7 @@ app.post('/api/capturepayment', async (req, res) => {
       const paymentIntent = await stripe.paymentIntents.capture(paymentIntentIds[key]);
       //console.log(paymentIntent);
       let response_object = {
+        payment_method : "Card",
         paymentIntentId : paymentIntent.id,
         status : paymentIntent.status
       }
@@ -70,6 +78,7 @@ app.post('/api/capturepayment', async (req, res) => {
     for (const key in paymentIntentIds) {
       const paymentIntent = await stripe.paymentIntents.cancel(paymentIntentIds[key]);
       let response_object = {
+        payment_method : "Card",
         paymentIntentId : paymentIntent.id,
         status : paymentIntent.status
       }
@@ -93,12 +102,127 @@ app.post('/api/capturepayment', async (req, res) => {
   });
 
   app.get('/api/transactionstatus', async (req, res) => {
-    // Making a single Successful Payment from Stripe API with Express Server has multiple steps
-    //console.log(req.body);
-    //console.log(req.query);
-    const paymentIntent = await stripe.paymentIntents.retrieve(req.query.payment_intent);
-    //console.log(paymentIntent);
     res.send('Redirecting Back ..... ');
+});
+app.get("/api/getkey", (req, res) =>
+  res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
+);
+// UPI Payment
+app.get('/api/makeupipayment', async (req, res) => {
+  const options = {
+    amount: Number(6 * 100),
+    currency: "INR",
+  };
+  const order = await razorpay.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+app.get('/api/razorpayPayment', async (req, res) => {
+  const orderId = req.query.order_id;
+  const key = req.query.key;
+  // Generate the Razorpay options based on the order ID
+  // Initialize Razorpay payment here and set the callback URL to your /api/verifyupipayment endpoint
+  res.send(`
+    <html>
+      <body>
+        <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+        <script>
+        const options = {
+          "key" : "${key}",
+          "amount": "600",
+          "currency": "INR",
+          "name": "Ajeet",
+          "description": "Payment",
+          "image": "",
+          "config" : {
+              "display": {
+            
+                "hide": [
+                  {
+                    "method":"card",
+                  },
+                  {
+                    "method":"wallet",
+                  },
+                  {
+                    "method":"netbanking",
+                  },
+                  {
+                    "method":"paylater",
+                  }
+                ],
+            
+                "sequence": ["block.code"], // The sequence in which blocks and methods should be shown
+            
+                "preferences": {
+                  "show_default_blocks": "true" // Should Checkout show its default blocks?
+                }
+              }
+            }, 
+          "callback_url": "http://localhost:3000/api/verifyupipayment",
+          "order_id": "${orderId}",
+          "prefill": {
+              "name": "Ajeet Verma",
+              "email": "ajeet@gmail.com",
+              "contact": "1234567890"
+          },
+          "notes": {
+              "address": "IIIT Naya Raipur"
+          },
+          "theme": {
+              "color": "#121212"
+          }
+      };
+        const razorpay = new window.Razorpay(options);
+        razorpay.open();
+        </script>
+      </body>
+    </html>
+  `);
+});
+
+
+app.post('/api/verifyupipayment', async (req, res) => {
+  console.log(req.body);
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
+    req.body;
+  console.log(razorpay_order_id, razorpay_payment_id, razorpay_signature);
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_API_SECRET)
+    .update(body.toString())
+    .digest("hex");
+  
+  const isAuthentic = expectedSignature === razorpay_signature;
+  console.log(isAuthentic);
+
+  if (isAuthentic) {
+
+    upiPaymentResult = {
+      success: true,
+      reference: razorpay_payment_id,
+    };
+    res.json(
+      {
+        success: true,
+        reference: razorpay_payment_id,
+        message: "Payment Successful! , Close the window to return to the application",
+      }
+    );
+  } else {
+    res.status(400).json({
+      success: false,
+    });
+  }
+});
+
+app.get('/api/upipaymentstatus', async (req, res) => {
+  res.json(upiPaymentResult);
 });
 
 
